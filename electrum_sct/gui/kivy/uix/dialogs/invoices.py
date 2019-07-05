04718cde58966a4fa0b50f226e6f261f00421ac2
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2019-07-06 00:38:58 +0100

    Update invoices.py

--- electrum_sct/gui/kivy/uix/dialogs/invoices.py
+++ electrum_sct/gui/kivy/uix/dialogs/invoices.py
@@ -17,7 +17,7 @@ Builder.load_string('''
     amount: ''
     status: ''
     date: ''
-    icon: 'atlas://electrum_nyc/gui/kivy/theming/light/important'
+    icon: 'atlas://electrum_sct/gui/kivy/theming/light/important'
     Image:
         id: icon
         source: root.icon
@@ -76,10 +76,10 @@ Builder.load_string('''
 ''')
 
 from kivy.properties import BooleanProperty
-from electrum_nyc.gui.kivy.i18n import _
-from electrum_nyc.util import format_time
-from electrum_nyc.paymentrequest import PR_UNPAID, PR_PAID, PR_UNKNOWN, PR_EXPIRED
-from electrum_nyc.gui.kivy.uix.context_menu import ContextMenu
+from electrum_sct.gui.kivy.i18n import _
+from electrum_sct.util import format_time
+from electrum_sct.paymentrequest import PR_UNPAID, PR_PAID, PR_UNKNOWN, PR_EXPIRED
+from electrum_sct.gui.kivy.uix.context_menu import ContextMenu
 
 invoice_text = {
     PR_UNPAID:_('Pending'),
@@ -88,10 +88,10 @@ invoice_text = {
     PR_EXPIRED:_('Expired')
 }
 pr_icon = {
-    PR_UNPAID: 'atlas://electrum_nyc/gui/kivy/theming/light/important',
-    PR_UNKNOWN: 'atlas://electrum_nyc/gui/kivy/theming/light/important',
-    PR_PAID: 'atlas://electrum_nyc/gui/kivy/theming/light/confirmed',
-    PR_EXPIRED: 'atlas://electrum_nyc/gui/kivy/theming/light/close'
+    PR_UNPAID: 'atlas://electrum_sct/gui/kivy/theming/light/important',
+    PR_UNKNOWN: 'atlas://electrum_sct/gui/kivy/theming/light/important',
+    PR_PAID: 'atlas://electrum_sct/gui/kivy/theming/light/confirmed',
+    PR_EXPIRED: 'atlas://electrum_sct/gui/kivy/theming/light/close'
 }
 
 
