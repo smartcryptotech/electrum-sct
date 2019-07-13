8a2969ca91353c73b47160f5e14fa4d3ca46a872
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2019-07-13 19:31:54 +0100

    Update qt.py

--- electrum_sct/plugins/ledger/qt.py
+++ electrum_sct/plugins/ledger/qt.py
@@ -5,10 +5,10 @@ from functools import partial
 from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtWidgets import QInputDialog, QLabel, QVBoxLayout, QLineEdit
 
-from electrum_nyc.i18n import _
-from electrum_nyc.plugin import hook
-from electrum_nyc.wallet import Standard_Wallet
-from electrum_nyc.gui.qt.util import WindowModalDialog
+from electrum_sct.i18n import _
+from electrum_sct.plugin import hook
+from electrum_sct.wallet import Standard_Wallet
+from electrum_sct.gui.qt.util import WindowModalDialog
 
 from .ledger import LedgerPlugin
 from ..hw_wallet.qt import QtHandlerBase, QtPluginBase
