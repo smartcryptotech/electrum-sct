69d7935a8b613e78a0bc9a8ce4264e6b3a13c8da
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2019-07-13 19:27:12 +0100

    Update qt.py

--- electrum_sct/plugins/labels/qt.py
+++ electrum_sct/plugins/labels/qt.py
@@ -5,9 +5,9 @@ import sys
 from PyQt5.QtCore import QObject, pyqtSignal
 from PyQt5.QtWidgets import (QHBoxLayout, QLabel, QVBoxLayout)
 
-from electrum_nyc.plugin import hook
-from electrum_nyc.i18n import _
-from electrum_nyc.gui.qt.util import ThreadedButton, Buttons, EnterButton, WindowModalDialog, OkButton
+from electrum_sct.plugin import hook
+from electrum_sct.i18n import _
+from electrum_sct.gui.qt.util import ThreadedButton, Buttons, EnterButton, WindowModalDialog, OkButton
 
 from .labels import LabelsPlugin
 
