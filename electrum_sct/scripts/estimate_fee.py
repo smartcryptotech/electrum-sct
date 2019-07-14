87c411f912582bec2469e470d4dc190a353d7cb3
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2019-07-14 17:03:27 +0100

    Update estimate_fee.py

--- electrum_sct/scripts/estimate_fee.py
+++ electrum_sct/scripts/estimate_fee.py
@@ -4,8 +4,8 @@ import asyncio
 from statistics import median
 from numbers import Number
 
-from electrum_nyc.network import filter_protocol, Network
-from electrum_nyc.util import create_and_start_event_loop, log_exceptions
+from electrum_sct.network import filter_protocol, Network
+from electrum_sct.util import create_and_start_event_loop, log_exceptions
 
 
 loop, stopping_fut, loop_thread = create_and_start_event_loop()
